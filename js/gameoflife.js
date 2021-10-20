e099583a57ae6e681469f4e89c36cb6029f8e711
moki-daniel <mokidnl@gmail.com>
2021-10-20 23:00:43 +0300

    Completed module

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -60,7 +60,13 @@ const getLivingNeighbors = (cell, state) => {
   return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
 };
 
-const willBeAlive = (cell, state) => {};
+const willBeAlive = (cell, state) => {
+  const livingNeighbors = getLivingNeighbors(cell, state);
+  return (
+    livingNeighbors.length == 3 ||
+    (contains.call(state, cell) && livingNeighbors.length === 2)
+  );
+};
 
 const calculateNext = (state) => {};
 
